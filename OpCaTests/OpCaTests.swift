7f54a929cf3368b8d8d0d536cc7a3b501830b08b
ihoflaz <hulusioflaz200@gmail.com>
2024-12-02 14:34:50 +0300

    Initial Commit

---
+++ OpCaTests/OpCaTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpCaTests.swift
+//  OpCaTests
+//
+//  Created by İbrahim Hulusi Oflaz on 2.12.2024.
+//
+
+import Testing
+@testable import OpCa
+
+struct OpCaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
